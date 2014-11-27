131d10c55b9567c2a8966bf4f9f4c578a1c2e0f0
pdigiglio <p.digiglio91@gmail.com>
2014-11-27 21:13:41 +0100

    improve documentation and implement the efficiency of the detector

--- radioactive_decay.C
+++ radioactive_decay.C
@@ -39,8 +39,8 @@ myGauss ( double *x, double *p ) {
 }
 
 	int
-main ( void ) {
-//radioactive_decay ( void ) {
+//main ( void ) {
+radioactive_decay ( void ) {
 
 	/// Initialize the seed for the random generator (the default argument is `0`).
 	gRandom->SetSeed();
@@ -61,12 +61,8 @@ main ( void ) {
 			2 );          /* parameters number */
 
 	/// Declare a `TF1` function containing the built-in `TMath::Gaus()` function.
-	TF1 *Func = new TF1(
-			"G",                       /* name */
-			"TMath::Gaus(x, 5, 1, 1)", /* what function to call */
-			- Infinity(),              /* minimum value (from) */
-			Infinity());               /* maximum value (to) */
-	
+	TF1 *Func = new TF1( "G", "TMath::Gaus(x, 5, 1, 1)", - Infinity(), Infinity());
+
 	/// To get the probability of a decay per second I evaluate
 	/// \f[
 	/// \int_{9.2}^\infty \frac{\mathrm{e}^{(x-5)^2\!/2}}{\sqrt{2\pi}\,\sigma} \,\mathrm{d}x 
@@ -88,7 +84,8 @@ main ( void ) {
 	const unsigned int time = 10000;
 
 	TH1I *ncl = new TH1I( "", "Radioactive decay;Time [s]; Nuclei", time, 0, time );
-	TH1I *decay = new TH1I( "", "Decays per second;N. of decays; Occurrences", 30, 0, 30 );
+	TH1I *decay = new TH1I( "", "Decays per second;N. of decays; Occurrences", 20, 0, 20 );
+	TH1I *dtc = new TH1I( "", "Alpha detected per second;N. of decays; Occurrences", 20, 0, 20 );
 
 	// take initial time
 	clock_t start = clock();
@@ -98,9 +95,9 @@ main ( void ) {
 	// start cycling over time
 	for ( unsigned int t = 1; t <= time; ++ t ) {
 //		cerr << t << endl;
-		cout << t << " "<< nuclei << " " << nuclei_err << endl;
+//		cout << t << " "<< nuclei << " " << nuclei_err << endl;
 
-		/// Fill the histogram `ncl` with the current value of `ncl` variable.
+		/// Fill the histogram `ncl` with the current value of `nuclei` counter.
 		ncl->SetBinContent( t, nuclei );
 
 		for ( n = nuclei; n > 0; -- n ) {
@@ -114,10 +111,26 @@ main ( void ) {
 		/// with this value.
 		decay->Fill( ncl->GetBinContent( t ) - nuclei );
 
-		// update the error
+		/// The error for the nuclei number is initially set to zero. Then I use the
+		/// propagation of the error using the formula \f$\sqrt{\delta x^2 + \delta y ^ 2}\f$.
+		/// Since che number of nuclei \f$n_j\f$ at the \f$j\f$-th step is \f$n_{j-1} - d_j\f$, where
+		/// \f$d_j\f$ is the number of decays at the step \f$j\f$, I can evaluate recursively the 
+		/// error as \f$\delta n_j^2 = \delta n_{j-1}^2 - \delta d_j^2\f$.
+		/// Since the decays are poisson distribuited, \f$\delta d_j^2 = d_j\f$.
 		nuclei_err *= nuclei_err;
 		nuclei_err += (double) ( ncl->GetBinContent( t ) - nuclei );
 		nuclei_err = sqrt( nuclei_err );
+
+		/// Modelize the detector which detects \f$60\,\f$\% of the alpha-particle emitted.
+		/// The number of decayd nuclei equals the number of \f$\alpha\f$-particles.
+		const unsigned short int efficiency = 6;
+		unsigned int detected = 0;
+		for ( unsigned int j = 0; j < ncl->GetBinContent( t ) - nuclei; ++ j ) {
+			if( gRandom->Integer(10) < efficiency )
+			++ detected;
+		}
+
+		dtc->Fill( detected );
 	}
 
 	cerr << "Time: " << (double) ( clock() - start ) / CLOCKS_PER_SEC << endl;
@@ -131,6 +144,10 @@ main ( void ) {
 	// set color line and draw line
 //	decay->SetLineColor( kRed );
 	decay->Draw("Csame");
+
+	new TCanvas();
+	dtc->Draw( "E" );
+	dtc->Draw( "Csame" );
 
 	return 0;
 }		/* -----  end of function radioactive_decay.C  ----- */
