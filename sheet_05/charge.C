8f665f07b977f850d0b76830af0e0a2045575b2c
pdigiglio <p.digiglio91@gmail.com>
2015-02-28 14:35:50 +0100

    two different histograms for filtered and not-filtered data

--- sheet_05/charge.C
+++ sheet_05/charge.C
@@ -15,6 +15,7 @@
 #include "TH1D.h"
 // To use cutting data condition.
 #include "TCut.h"
+#include "TCanvas.h"
 
 /**
  * @brief The main function of the ROOT script.
@@ -46,6 +47,7 @@ charge ( const char inFileName[] = "chargedata.txt",
     /// Create a dumb histogram to evaluate mean and root mean square. This is useful
 	/// because then I can access them via `TH1::GetMean()` and `TH1::GetRMS()` methods.
     TH1D *histogram = new TH1D( "histo", "Charge not cutted", bins + 2 , min - binWidth, max + binWidth );
+    TH1D *histogram2 = new TH1D( "histo2", "Charge cutted", bins + 2 , min - binWidth, max + binWidth );
 
     /// Draw the branch filling my histogram (this will evaluate mean and RMS).
     tree->Draw( "charge >> histo" );
@@ -55,8 +57,10 @@ charge ( const char inFileName[] = "chargedata.txt",
 
     /// I add a filter for my data, ignoring all values greater than \f$30\,\mbox{nC}\f$.
 	TCut chargeCut = "charge < 30";
-	/// Then I plot and process them using the same histogram to get mean and RMS.
-	tree->Draw( "charge >> histo", chargeCut );
+
+	new TCanvas;
+	/** Then I plot and process them using the same histogram to get mean and RMS. */
+	tree->Draw( "charge >> histo2", chargeCut );
 
 	cout << " ~~~ FILTER RESULTS ~~~ " << endl;
     cout << "Mean: " << histogram->GetMean() << " RMS: " << histogram->GetRMS() << endl;
